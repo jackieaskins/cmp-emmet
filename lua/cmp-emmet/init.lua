b3763637b9a1a72548a341236d0609ebadf29011
Jacqueline Askins <askinsjacqueline@gmail.com>
2022-06-26 12:30:28 -0400

    Update to use vscode-emmet-helper

--- lua/cmp-emmet/init.lua
+++ lua/cmp-emmet/init.lua
@@ -1,4 +1,13 @@
-local prefix = '~'
+local filetype_syntax_map = {
+  html = 'html',
+  xml = 'xml',
+  typescriptreact = 'jsx',
+  javascriptreact = 'jsx',
+  css = 'css',
+  sass = 'css',
+  scss = 'css',
+  less = 'css',
+}
 
 local source = {}
 
@@ -17,28 +26,36 @@ function source:get_keyword_pattern()
   return '.'
 end
 
-function source:get_trigger_characters()
-  return { '~' }
-end
-
 function source:complete(params, callback)
   local results = {}
 
+  local context = params.context
+  local bufnr = context.bufnr
+  local cursor = context.cursor
+  local filetype = context.filetype
+
   vim.fn.jobstart({
     'cmp-emmet',
-    '--params',
-    vim.fn.json_encode(params),
-    '--prefix',
-    prefix,
+    '--content',
+    table.concat(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false), '\n'),
+    '--languageId',
+    filetype,
+    '--position',
+    vim.fn.json_encode({ line = cursor.line, character = cursor.character }),
+    '--syntax',
+    filetype_syntax_map[filetype],
+    '--uri',
+    vim.uri_from_bufnr(bufnr),
   }, {
     on_stdout = function(_, data)
       local output = data[1]
       if output ~= '' then
-        table.insert(results, vim.fn.json_decode(output))
+        results = vim.fn.json_decode(output)
       end
     end,
     on_exit = function()
-      callback(#results > 0 and results or nil)
+      source.incomplete = results.isIncomplete or false
+      callback(results and results.items or nil)
     end,
   })
 end
--- src/cli.ts
+++ src/cli.ts
@@ -1,94 +1,29 @@
 #!/usr/bin/env node
 
-import expandAbbreviation, { extract, resolveConfig, SyntaxType } from "emmet";
+import { doComplete } from "vscode-emmet-helper";
+import { TextDocument } from "vscode-languageserver-textdocument";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 
-interface CompletionParams {
-  context: {
-    cursor: {
-      character: number;
-      col: number;
-      line: number;
-      row: number;
-    };
-    cursor_before_line: string;
-    cursor_line: string;
-    filetype: string;
-  };
-}
-
-const SNIPPET_KIND = 15;
-const SNIPPET_FORMAT = 2;
-const FILETYPE_MAP: Record<string, { syntax: string; type: SyntaxType }> = {
-  html: { syntax: "html", type: "markup" },
-  xml: { syntax: "xml", type: "markup" },
-  typescriptreact: { syntax: "jsx", type: "markup" },
-  javascriptreact: { syntax: "jsx", type: "markup" },
-  css: { syntax: "css", type: "stylesheet" },
-  sass: { syntax: "css", type: "stylesheet" },
-  scss: { syntax: "css", type: "stylesheet" },
-  less: { syntax: "css", type: "stylesheet" },
-};
-
-const { params, prefix } = yargs(hideBin(process.argv))
+const { content, languageId, position, syntax, uri } = yargs(
+  hideBin(process.argv)
+)
   .options({
-    params: { type: "string", demand: true },
-    prefix: { type: "string" },
+    content: { type: "string", demand: true },
+    languageId: { type: "string", demand: true },
+    position: { type: "string", demand: true },
+    syntax: { type: "string", demand: true },
+    uri: { type: "string", demand: true },
   })
   .parseSync();
 
-const {
-  context: {
-    filetype,
-    cursor_line: cursorLine,
-    cursor: { character, line },
-  },
-} = JSON.parse(params) as CompletionParams;
+const document = TextDocument.create(uri, languageId, 0, content);
+const result = doComplete(document, JSON.parse(position), syntax, {
+  showExpandedAbbreviation: "always",
+  showAbbreviationSuggestions: true,
+  showSuggestionsAsSnippets: true,
+});
 
-function parse() {
-  const { syntax, type } = FILETYPE_MAP[filetype];
-  const extracted = extract(cursorLine, character, { type, prefix });
-
-  if (!extracted) {
-    return;
-  }
-
-  const { abbreviation, start, end } = extracted;
-  const emmetConfig = resolveConfig({
-    syntax,
-    type,
-    options: {
-      "output.field": (index, placeholder) =>
-        `$\{${index}${placeholder ? ":" + placeholder : ""}}`,
-    },
-  });
-
-  const snippets = new Set([
-    abbreviation,
-    ...Object.keys(emmetConfig.snippets),
-  ]);
-
-  Array.from(snippets).forEach((snippet) => {
-    const expanded = expandAbbreviation(snippet, emmetConfig);
-
-    console.log(
-      JSON.stringify({
-        label: `~${snippet}`,
-        insertTextFormat: SNIPPET_FORMAT,
-        detail: snippet,
-        documentation: { kind: "markdown", value: expanded },
-        textEdit: {
-          range: {
-            start: { line, character: start },
-            end: { line, character: end },
-          },
-          newText: expanded,
-        },
-        kind: SNIPPET_KIND,
-      })
-    );
-  });
+if (result) {
+  console.log(JSON.stringify(result));
 }
-
-parse();
